bf50360d190f1e62506b41df7b4df0e8cc85c885
JPayne19x <67328877+JPayne19x@users.noreply.github.com>
2021-02-24 14:35:54 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//JJared Payne 8.10.20
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -11,44 +10,70 @@ var mysql = require('mysql');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(session({secret: 'SuperSecretPassword'}));
-app.use('/css',express.static('css'));
-app.use('/images',express.static('images'));
-app.use('/vendor',express.static('vendor'));
-app.use('/js',express.static('js'));
+app.use(express.static('public'));
+app.use(express.static('js'));
+app.use(express.static('css'));
+app.use(express.static('jquery'));
+app.use(express.static('images'));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 8916);
+
+//Create mysql pool
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_payneje',
+    password: 'Seattleites',
+    database: 'cs340_payneje'
+});
 
 module.exports.pool = pool;
 
 app.get('/', function (req, res, next) {
   res.render('index');
 });
+
 app.get('/index.html', function (req, res, next) {
   res.render('index');
-});
-
-app.get('/profile.html', function (req, res, next) {
-  res.render('profile');
-});
-
-app.get('/projectlist.html',function(req,res,next){
-  res.render('projectlist');
 });
 
 app.get('/registeremployee.html',function(req,res,next){
   res.render('registeremployee');
 });
 
+app.get('/profile.html', function (req, res, next) {
+  let context = {};
+  //SELECT all employee profiles when page is first loaded.
+  if (!req.session.active) {
+          var createString = "SELECT *" + "FROM Employees;"
+          pool.query(createString, function (err) {
+              context.results = "* Employees Selected ";
+              res.render('profile'); //render handlebars
+              req.session.active = 'yes';
+              console.log('* Employees Selected console');
+          });
+      return;
+  }
+  //Renders the page each subsequent time the page is loaded
+  else {
+    console.log('else profile');
+      res.render('profile', context);
+  }
+});
+
+app.get('/projectlist.html',function(req,res,next){
+  res.render('projectlist');
+});
+
 app.get('/tasklist.html',function(req,res,next){
   res.render('tasklist');
 });
 
-
 app.listen(app.get('port'), function () {
-    console.log('Express started on http://flip3.engr.oregonstate.edu' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+  console.log('Express started on http://flip3.engr.oregonstate.edu' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
 
 app.use(function(req,res){
   res.status(404);
