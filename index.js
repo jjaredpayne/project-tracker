dc993762b26f067e571bfbd5a03268ed7a6d36f8
Sarah Bell <bellsar@oregonstate.edu>
2021-03-04 19:42:27 -0800

    mering mySQL and index

--- index.js
+++ index.js
@@ -25,6 +25,41 @@ var deleteManagedProjects = "DELETE ManagedProjects FROM ManagedProjects WHERE M
 var deleteManager = "DELETE FROM Managers WHERE Managers.employeeID = ";
 var deleteAssignedProjects = "DELETE AssignedTasks FROM AssignedTasks WHERE AssignedTasks.developerID IN (SELECT Developers.developerID FROM Developers WHERE Developers.employeeID = ";
 var deleteDeveloper = "DELETE FROM Developers WHERE Developers.employeeID = ";
+const getProjectQuery =`SELECT 
+    projectID, title, CONCAT(Employees.firstname, ' ', Employees.lastname) AS owner, percentComplete, projectStatus AS status, plannedEnd AS dueDate
+  FROM Projects 
+  JOIN ManagedProjects ON
+    ManagedProjects.projectID = Projects.projectID
+  JOIN Managers ON
+    Managers.managerID = ManagedProjects.managerID
+  JOIN Employees ON
+    Managers.employeeID = Employees.employeeID`;
+const getTaskQuery = `SELECT
+  taskID,
+  title,
+  taskDetails,
+  AssignedTasks.developerID AS "developerID",
+  CONCAT(Employees.firstName, ' ', Employees.lastName) AS "owner", 
+  dueDate,
+  AssignedTasks.satisfaction AS "satisfaction"
+FROM Tasks 
+JOIN AssignedTasks ON
+  AssignedTasks.taskID = Tasks.taskID
+JOIN Developers ON
+  Developers.developerID = AssignedTasks.developerID
+JOIN Employees ON
+  Employees.employeeID = Developers.employeeID
+WHERE projectID = ?
+  AND completed = 0`;
+const getProjectIDQuery = "SELECT projectID FROM Projects where title=?;"
+const getManagersQuery = "SELECT managerID, CONCAT(Employees.firstname, ' ', Employees.lastname) AS name FROM Managers JOIN Employees ON Managers.employeeID = Employees.employeeID"
+const insertProjectQuery = "INSERT INTO Projects(`title`, `percentComplete`, `plannedEnd`, `projectStatus`) VALUES (?,?,?,?) ";
+const insertManagedProjectQuery = "INSERT INTO ManagedProjects (`projectID`, `managerID`) VALUES (?,?) ";
+const insertTaskQuery = "INSERT INTO Tasks (`projectID`, `title`, `taskDetails`, `dueDate`) VALUES (?,?,?,?) ";
+const deleteProjectQuery = "DELETE FROM Projects WHERE projectID=? ";
+const deleteTaskQuery = "DELETE FROM Tasks WHERE taskID=? ";
+const updateProjectQuery = "UPDATE Projects SET title=?, percentComplete=?, plannedEnd=?, projectStatus=? WHERE projectID=? ";
+const updateTaskQuery = "UPDATE Tasks SET title=?, taskDetails=?, dueDate=?, completed=? WHERE taskID=? ";
 
 
 //Create mysql pool
@@ -131,22 +166,6 @@ app.post('/displayEmployees', function (req, res, next) {
   }
 });
 
-// app.post('/displayAll', function (req, res, next) {
-//   let context = {};
-//   if(req.body.workRole === "1"){
-//     pool.query(selectDevelopers, function (err, rows, results) {
-//       console.log(rows)
-//       context.sqlresults = JSON.parse(JSON.stringify(rows));
-//       res.render('profile', context);
-//     });
-//   }
-//   else{
-//     pool.query(selectManagers, function (err, rows, fields) {
-//       context.sqlresults = JSON.parse(JSON.stringify(rows));
-//       res.render('profile', context);
-//     });
-//   }
-// });
 
 app.post('/deleteDeveloper', function (req, res, next) {
   let context = {};
@@ -214,14 +233,138 @@ app.get('/insertdeveloper', function (req, res, next) {
   });
 });
 
-
-
 app.get('/projectlist.html',function(req,res,next){
-  res.render('projectlist');
+  var context = {};
+  mysql.pool.query(getProjectQuery, (err, rows, fields) => {
+    if(err){
+      next(err);
+      return;
+    }
+    context.Project = JSON.stringify(rows);
+  
+  mysql.pool.query(getManagersQuery, (err, rows, fields) => {
+    if(err){
+      next(err);
+      return;
+    }
+    context.Managers = JSON.stringify(rows)
+  })
+  res.render('projectlist', context);
+  });
 });
 
-app.get('/tasklist.html',function(req,res,next){
-  res.render('tasklist');
+app.get('/project.html',function(req,res,next){
+  var context = {};
+  mysql.pool.query(getTaskQuery, req.query.id, (err, rows, fields) => {
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.send(context);
+  });
+});
+
+app.post('/projectlist.html',function(req,res,next){
+  var {title, percentComplete, plannedEnd, projectStatus, userID} = req.body;
+  mysql.pool.query(insertProjectQuery, 
+    [title, percentComplete, plannedEnd, projectStatus], 
+    (err, result) =>{
+    if(err){
+      next(err);
+      return;
+    }
+  });
+
+    mysql.pool.query(getProjectIDQuery, title, (err, rows, fields) => {
+      if(err){
+        next(err);
+        return;
+      }
+      context.results = rows;
+  
+    mysql.pool.query(insertManagedProjectQuery, 
+      context.results, userID,
+      (err, result) =>{
+      if(err){
+        next(err);
+        return;
+      }
+
+      });
+    });
+
+    console.log("Sending result.");
+    res.send({result: result});
+});
+
+app.post('/project.html',function(req,res,next){
+  var {projectID, title, taskDetails, dueDate} = req.body;
+  mysql.pool.query(insertTaskQuery, 
+    [projectID, title, taskDetails, dueDate], 
+    (err, result) =>{
+    if(err){
+      next(err);
+      return;
+    }
+    //getAllData();
+  });
+});
+
+app.delete('/projectlist.html',function(req,res,next){
+  var context = {};
+  var {id} = req.body;
+  mysql.pool.query(deleteProjectQuery, [id], (err, result) => {
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Deleted " + result.changedRows + " rows.";
+    res.send(context);
+  });
+});
+
+app.delete('/project.html',function(req,res,next){
+  var context = {};
+  var {id} = req.body;
+  mysql.pool.query(deleteTaskQuery, [id], (err, result) => {
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Deleted " + result.changedRows + " rows.";
+    res.send(context);
+  });
+});
+
+app.put('/projectlist.html',function(req,res,next){
+  var context = {};
+  var {title, percentComplete, plannedEnd, projectStatus, id} = req.body;
+  mysql.pool.query(updateProjectQuery,
+    [title, percentComplete, plannedEnd, projectStatus, id],
+    (err, result) =>{
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Updated " + result.changedRows + " rows.";
+    res.send(context);
+  });
+});
+
+app.put('/project.html',function(req,res,next){
+  var context = {};
+  var {title, taskDetails, dueDate, completed, id} = req.body;
+  mysql.pool.query(updateTaskQuery,
+    [title, taskDetails, dueDate, completed, id],
+    (err, result) =>{
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Updated " + result.changedRows + " rows.";
+    res.send(context);
+  });
 });
 
 app.listen(app.get('port'), function () {
